bcc4670f97574c91ca2d262709ab82bff3b357d7
dynobo <dynobo@mailbox.org>
2026-02-06 13:01:16 +0100

    feat: update script adjusts manifest yaml

--- update.sh
+++ update.sh
@@ -1,9 +1,30 @@
 #!/bin/bash
-echo -n "Make sure to update the version in io.github.sphereview.yml first!"
+manifest_yaml="io.github.dynobo.sphereview.yml"
+repo_url="https://github.com/dynobo/sphereview.git"
+
 echo -n "Please enter a version to update to (v0.1.1): "
 read version
-git clone --depth 1 --branch $version https://github.com/dynobo/sphereview.git
+
+# Fetch the commit hash for the given tag
+commit_hash=$(git ls-remote "$repo_url" "refs/tags/$version" | cut -f1)
+if [ -z "$commit_hash" ]; then
+    echo "Error: Could not find commit hash for tag $version"
+    exit 1
+fi
+
+# Update yaml
+sed -i \
+    -e "/name: sphereview/,/^  - name:/ {
+        s/tag: v[0-9.]\+/tag: $version/
+        s/commit: [0-9a-f]\+/commit: $commit_hash/
+    }" \
+    "$manifest_yaml"
+
+
+# Update dependencies
+git clone --depth 1 --branch $version $repo_url
 flatpak-cargo-generator ./sphereview/Cargo.lock -o cargo-sources.json
 flatpak-node-generator npm ./sphereview/resources/photosphereviewer/package-lock.json -o node-sources.json
 rm -rf ./sphereview
+
 git add .
